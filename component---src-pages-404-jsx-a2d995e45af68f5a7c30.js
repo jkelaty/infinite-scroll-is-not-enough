d52223f101a6a974cd90711ff5057af7b9447cb5
jkelaty <jkelaty@icloud.com>
2020-08-03 17:08:02 -0400

    Updates

--- component---src-pages-404-jsx-a2d995e45af68f5a7c30.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{pssB:function(n,t,c){"use strict";c.r(t);var e=c("q1tI"),u=c("Wbzz");t.default=function(){return Object(e.useEffect)((function(){Object(u.b)("/")}),[]),null}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-a2d995e45af68f5a7c30.js.map
