e57c5501dfceddfa4b0a033febb1163311717281
jkelaty <jkelaty@icloud.com>
2020-08-05 20:45:42 -0400

    Backend update

---
+++ heroku/credentials.py
@@ -0,0 +1,11 @@
+import os
+
+TWITTER_CREDENTIALS = {
+    'API_KEY': os.environ['TWITTER_KEY'],
+    'API_SECRET': os.environ['TWITTER_SECRET']
+}
+
+GPT_2_CREDENTIALS = {
+    'TOKEN': os.environ['GPT_2_TOKEN']
+}
+
